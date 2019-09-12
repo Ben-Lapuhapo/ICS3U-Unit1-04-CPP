83597054d95508bd7634fec450c8db6e322d9f4b
Ben Lapuhapo <ben.lapuhapo@mths.ca>
2019-09-12 17:29:30 +0000

    Added rectangle calculator

---
+++ rectangle_calculator.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2019 St. Mother Teresa HS All Rights Reserved.
+//
+// Created By: Ben Lapuhapo.
+// Created on: Sep 2019.
+// This program calculates the area and perimeter of a rectangle
+
+#include <iostream>
+#include <cmath> // You need to add this library for pow() function
+
+int main() {
+    std::cout << "The rectangle's dimensions are L= 5, W= 3 " << std::endl;
+    std::cout << "The rectangle's perimeter is " << 2*5+2*3 << std::endl;
+    std::cout << "The rectangle's area is " << 5*3 << std::endl;
+}
